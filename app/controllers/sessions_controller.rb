613d0458054090027f1b43bbfe2cc6145e388d6f
Harika2153 <idamakanti.venkataharika@freshworks.com>
2022-06-20 15:45:10 +0530

    gaveusertype

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,11 @@ class SessionsController < ApplicationController
     end
     def create
         user = User.find_by(email: params[:email])
+        user.owner?
         if user.present? && user.authenticate(params[:password])
             session[:user_id] = user.id
             redirect_to root_path, notice: "Logged in successfully"
+            
         else
             flash[:alert] = "Ivalid email or password"
             render :new
