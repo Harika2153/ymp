e66e2c2d6810ec373bd5e325012ad16bf6004232
Harika2153 <idamakanti.venkataharika@freshworks.com>
2022-06-21 10:18:00 +0530

    owneruser

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,9 +1,11 @@
 class MainController < ApplicationController
     def index
+      if session[:user_id]
+        @user = User.find(session[:user_id])
+      end        
+      #if params[:user_type] == 1
+        
       
-      @user = User.find_by(params[:user_type])
-      
-
 
 
       
