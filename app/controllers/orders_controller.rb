3c247f3ef2ceeec0b13ecc43b3b932dd6267c72c
Harika2153 <idamakanti.venkataharika@freshworks.com>
2022-06-22 11:30:41 +0530

    orderstable

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,20 @@
+class OrdersController < ApplicationController
+    def new
+        @order = Order.new
+      end
+    def create
+        @order = Order.new#(order_params)
+    
+        respond_to do |format|
+          if @order.save
+            format.html { redirect_to order_path, notice: 'Order was successfully created.' }
+            
+          else
+            format.html { render :new }
+            
+          end
+        end
+      end
+    
+    
+end
---
+++ db/migrate/20220621094613_orders.rb
@@ -0,0 +1,9 @@
+class Orders < ActiveRecord::Migration[7.0]
+  def change
+    create_table :orders do |t|
+      t.string :name
+      
+      t.timestamps
+    end
+  end
+end
